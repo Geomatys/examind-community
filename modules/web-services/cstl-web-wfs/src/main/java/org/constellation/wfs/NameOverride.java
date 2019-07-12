57e42de0a692195958fe83993bdf2853ac604f10
jsorel <johann.sorel@geomatys.com>
2019-07-12 11:59:44 +0200

    feat(Storage): NameOverride support FeatureSet

--- modules/web-services/cstl-web-wfs/src/main/java/org/constellation/wfs/NameOverride.java
+++ modules/web-services/cstl-web-wfs/src/main/java/org/constellation/wfs/NameOverride.java
@@ -5,8 +5,15 @@ package org.constellation.wfs;
  * @author Alexis Manin (Geomatys)
  */
 
+import java.util.Optional;
+import java.util.stream.Stream;
 import org.apache.sis.feature.AbstractFeature;
 import org.apache.sis.feature.builder.FeatureTypeBuilder;
+import org.apache.sis.metadata.iso.DefaultMetadata;
+import org.apache.sis.storage.DataStoreException;
+import org.apache.sis.storage.FeatureSet;
+import org.apache.sis.storage.event.ChangeEvent;
+import org.apache.sis.storage.event.ChangeListener;
 import org.apache.sis.util.Static;
 import org.geotoolkit.data.FeatureCollection;
 import org.geotoolkit.data.FeatureStoreRuntimeException;
@@ -15,6 +22,8 @@ import org.opengis.feature.Feature;
 import org.opengis.feature.FeatureType;
 import org.opengis.feature.Property;
 import org.opengis.feature.PropertyNotFoundException;
+import org.opengis.geometry.Envelope;
+import org.opengis.metadata.Metadata;
 import org.opengis.util.GenericName;
 
 public class NameOverride extends Static {
@@ -81,6 +90,63 @@ public class NameOverride extends Static {
         }
     }
 
+    private static class NameOverrideFeatureSet implements FeatureSet {
+
+        private final FeatureSet original;
+        private final FeatureType originalType;
+        private final FeatureType nameOverride;
+
+        public NameOverrideFeatureSet(FeatureSet originalFC, final GenericName name) throws DataStoreException {
+            this.original = originalFC;
+            this.originalType = originalFC.getType();
+            this.nameOverride = wrap(originalType, name);
+        }
+
+        @Override
+        public FeatureType getType() {
+            return nameOverride;
+        }
+
+        private Feature modify(Feature original) throws FeatureStoreRuntimeException {
+            final FeatureType targetType;
+            final FeatureType currentType = original.getType();
+            if (originalType.equals(currentType)) {
+                targetType = nameOverride;
+            } else {
+                targetType = wrap(currentType, nameOverride.getName());
+            }
+            return new SimpleDecoratedFeature(original, targetType);
+        }
+
+        @Override
+        public Stream<Feature> features(boolean parallel) throws DataStoreException {
+            return original.features(parallel).map(this::modify);
+        }
+
+        @Override
+        public Optional<Envelope> getEnvelope() throws DataStoreException {
+            return original.getEnvelope();
+        }
+
+        @Override
+        public Optional<GenericName> getIdentifier() throws DataStoreException {
+            return Optional.of(getType().getName());
+        }
+
+        @Override
+        public Metadata getMetadata() throws DataStoreException {
+            return new DefaultMetadata();
+        }
+
+        @Override
+        public <T extends ChangeEvent> void addListener(ChangeListener<? super T> listener, Class<T> eventType) {
+        }
+
+        @Override
+        public <T extends ChangeEvent> void removeListener(ChangeListener<? super T> listener, Class<T> eventType) {
+        }
+    }
+
     public static FeatureType wrap(final FeatureType toWrap, final GenericName newName) {
         final FeatureTypeBuilder ftb = new FeatureTypeBuilder(toWrap);
         ftb.setName(newName);
@@ -93,5 +159,9 @@ public class NameOverride extends Static {
 
     public static FeatureCollection wrap(final FeatureCollection source, final GenericName newName) {
         return new NameOverrideCollection(source, newName);
+    }
+
+    public static FeatureSet wrap(final FeatureSet source, final GenericName newName) throws DataStoreException {
+        return new NameOverrideFeatureSet(source, newName);
     }
 }
