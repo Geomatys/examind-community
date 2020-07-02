eca73355e27d39d70131286be3ce3a2a17d114c6
Hilmi BOUALLAGUE <hilmi.bouallegue@geomatys.com>
2020-07-02 17:10:03 +0200

    fix(webui): fix delete multi-selected  data

--- modules/bundles/exa-webui/grunt/src/js/data/dashboard/dashboard-datasetListing.controller.js
+++ modules/bundles/exa-webui/grunt/src/js/data/dashboard/dashboard-datasetListing.controller.js
@@ -11,7 +11,7 @@ angular.module('cstl-data-dashboard')
  * @param {DatasetDashboard} DatasetDashboard
  * @constructor
  */
-function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, defaultDatasetQuery, Dataset, DatasetDashboard) {
+function DatasetListingController($rootScope, $scope, DashboardHelper, Examind, defaultDatasetQuery, Dataset, DatasetDashboard) {
 
     var self = this;
 
@@ -21,25 +21,25 @@ function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, d
     Object.defineProperties(self, {
         dataset: {
             enumerable: true,
-            get: function() {
+            get: function () {
                 return DatasetDashboard.dataset;
             },
-            set: function(dataset) {
+            set: function (dataset) {
                 return (DatasetDashboard.dataset = dataset);
             }
         },
         data: {
             enumerable: true,
-            get: function() {
+            get: function () {
                 return DatasetDashboard.data;
             },
-            set: function(data) {
+            set: function (data) {
                 return (DatasetDashboard.data = data);
             }
         }
     });
 
-    //reset selection 
+    //reset selection
     self.dataset = null;
     self.data = null;
 
@@ -47,21 +47,21 @@ function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, d
     self.search();
 
     // Method used to modify a query filter and launch the search.
-    self.setFilter = function(key, value) {
+    self.setFilter = function (key, value) {
         self.removeFilter(self.query, key);
-        self.query.filters.push({"field":key,"value":value});
+        self.query.filters.push({"field": key, "value": value});
     };
 
-    self.existFilter = function(field) {
+    self.existFilter = function (field) {
         return angular.isDefined(self.getFilter(field));
     };
 
-    self.checkFilter = function(field, value) {
+    self.checkFilter = function (field, value) {
         var filter = self.getFilter(field);
         return angular.isDefined(filter) && filter.value === value;
     };
 
-    self.removeFilter = function(query, field) {
+    self.removeFilter = function (query, field) {
         if (query.filters) {
             for (var i = 0; i < query.filters.length; i++) {
                 if (query.filters[i].field === field) {
@@ -72,7 +72,7 @@ function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, d
         }
     };
 
-    self.getFilter = function(field) {
+    self.getFilter = function (field) {
         if (self.query.filters) {
             for (var i = 0; i < self.query.filters.length; i++) {
                 if (self.query.filters[i].field === field) {
@@ -84,16 +84,15 @@ function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, d
     };
 
 
-
     // Method used to filter dataset on their data types.
-    self.setTypeFilter = function(type) {
+    self.setTypeFilter = function (type) {
         switch (type) {
             case 'VECTOR':
-                self.query.filters.push({"field":"hasVectorData","value":"true"});
+                self.query.filters.push({"field": "hasVectorData", "value": "true"});
                 self.removeFilter(self.query, "hasCoverageData");
                 break;
             case 'COVERAGE':
-                self.query.filters.push({"field":"hasCoverageData","value":"true"});
+                self.query.filters.push({"field": "hasCoverageData", "value": "true"});
                 self.removeFilter(self.query, "hasVectorData");
                 break;
             default:
@@ -104,7 +103,7 @@ function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, d
     };
 
     // Method used to reset the search criteria.
-    self.resetCriteria = function() {
+    self.resetCriteria = function () {
         //reset selection
         self.dataset = null;
         self.data = null;
@@ -119,30 +118,45 @@ function DatasetListingController($rootScope,$scope, DashboardHelper, Examind, d
     // Observe the 'reloadDatasets' event to re-launch the search.
     $scope.$on('reloadDatasets', self.search);
 
-    self.deleteData = function() {
-        return Dataset.deleteData(self.data,
-            function(){
-                self.data = null;
+    self.deleteData = function () {
+        return Dataset.deleteData(self.data[0],
+            function () {
+                self.data.splice(0, 1);
                 return self.setPage(1);
             })
-            .catch(function(err) {
-                if(err === 'cancel') {
+            .catch(function (err) {
+                if (err === 'cancel') {
                     return;
                 }
                 // TODO : Move real error management (Growl???) here
-                console.error("Unable to delete this data.", self.data, err);
+                console.error("Unable to delete this data.", self.data[0], err);
             });
     };
 
-    self.deleteDataset = function() {
-        return Dataset.deleteDataset(self.dataset,
-            function() {
-                self.dataset = null;
-                self.data = null;
+    self.deleteMultiData = function () {
+        return Dataset.deleteMultiData(self.data,
+            function () {
+                self.data = [];
                 return self.setPage(1);
             })
-            .catch(function(err) {
-                if(err === 'cancel') {
+            .catch(function (err) {
+                if (err === 'cancel') {
+                    return;
+                }
+                // TODO : Move real error management (Growl???) here
+                console.error("Unable to delete this data.", err);
+            });
+    };
+
+    self.deleteDataset = function () {
+        return Dataset.deleteDataset(self.dataset,
+            function () {
+                self.dataset = null;
+                self.data = [];
+                return self.setPage(1);
+            })
+            .catch(function (err) {
+                if (err === 'cancel') {
                     return;
                 }
                 // TODO : Move real error management (Growl???) here
