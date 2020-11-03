955715cc7f19b81da1750050bd7913b0f3f7d3c0
glegal <guilhem.legal@geomatys.com>
2020-11-03 11:07:50 +0100

    fix(service): fix sts date format

--- modules/web-services/cstl-web-sts/src/main/java/com/examind/sts/ws/rs/STSResponseWriter.java
+++ modules/web-services/cstl-web-sts/src/main/java/com/examind/sts/ws/rs/STSResponseWriter.java
@@ -47,7 +47,7 @@ public class STSResponseWriter implements HttpMessageConverter<STSResponse> {
 
     private static final Logger LOGGER = Logging.getLogger("com.examind.sts.ws.rs");
 
-    private static final SimpleDateFormat DATE_FORM = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:SS'Z'");
+    private static final SimpleDateFormat DATE_FORM = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
     static {
         DATE_FORM.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
