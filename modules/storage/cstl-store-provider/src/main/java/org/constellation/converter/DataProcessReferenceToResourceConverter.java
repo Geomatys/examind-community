c10a79438d45e7902f0ffadb401569fa7459886a
Alexis Manin <alexis.manin@geomatys.com>
2021-10-20 17:33:22 +0200

    feat(Storage): improve data process reference converter
    allow to define data reference with data id only

--- modules/storage/cstl-store-provider/src/main/java/org/constellation/converter/DataProcessReferenceToResourceConverter.java
+++ modules/storage/cstl-store-provider/src/main/java/org/constellation/converter/DataProcessReferenceToResourceConverter.java
@@ -2,8 +2,11 @@ package org.constellation.converter;
 
 import org.apache.sis.storage.Resource;
 import org.apache.sis.util.UnconvertibleObjectException;
+import org.constellation.admin.SpringHelper;
+import org.constellation.business.IDataBusiness;
 import org.constellation.dto.process.DataProcessReference;
 import org.constellation.exception.ConstellationException;
+import org.constellation.exception.TargetNotFoundException;
 import org.constellation.provider.Data;
 import org.constellation.provider.DataProvider;
 import org.constellation.provider.DataProviders;
@@ -29,16 +32,31 @@ public class DataProcessReferenceToResourceConverter extends SimpleConverter<Dat
      */
     @Override
     public Resource apply(DataProcessReference ref) throws UnconvertibleObjectException {
-        final Resource source;
+        if (ref == null) return null;
         try {
-            DataProvider dp = DataProviders.getProvider(ref.getProvider());
-            Data d = dp.get(ref.getNamespace(), ref.getName());
-            if (d != null) {
-                return d.getOrigin();
-            }
-            throw new UnconvertibleObjectException("Unabel to find a data correspounding to the reference");
+            return getByDataId(ref);
         } catch (ConstellationException e) {
-            throw new UnconvertibleObjectException(e);
+            // Maybe input reference does not give data id. We'll try with provider information instead
+            try {
+                return findData(ref.getProvider(), ref.getNamespace(), ref.getName());
+            } catch (ConstellationException bis) {
+                e.addSuppressed(bis);
+                throw new UnconvertibleObjectException("Cannot find data for given information", e);
+            }
         }
+    }
+
+    private static Resource getByDataId(final DataProcessReference ref) throws ConstellationException {
+        final IDataBusiness dataBiz = SpringHelper.getBean(IDataBusiness.class);
+        if (dataBiz == null) throw new UnconvertibleObjectException("Application context unavailable");
+        final org.constellation.dto.Data data = dataBiz.getData(ref.getId());
+        return findData(data.getProviderId(), data.getNamespace(), data.getName());
+    }
+
+    private static Resource findData(int providerId, String namespace, String name) throws ConstellationException {
+        DataProvider dp = DataProviders.getProvider(providerId);
+        Data d = dp.get(namespace, name);
+        if (d == null) throw new TargetNotFoundException(String.format("No data found in provider %s for name %s:%s", providerId, namespace, name));
+        return d.getOrigin();
     }
 }
