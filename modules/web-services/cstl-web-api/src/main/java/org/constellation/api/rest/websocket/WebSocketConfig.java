8837551eb663081d895a0aef2f0d0ab8d459e802
Alexis Manin <alexis.manin@geomatys.com>
2023-07-04 11:24:22 +0200

    chore(Build): fix WebSocket configuration API due to Spring upgrade

--- modules/web-services/cstl-web-api/src/main/java/org/constellation/api/rest/websocket/WebSocketConfig.java
+++ modules/web-services/cstl-web-api/src/main/java/org/constellation/api/rest/websocket/WebSocketConfig.java
@@ -22,15 +22,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.StompWebSocketEndpointRegistration;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @EnableScheduling
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
