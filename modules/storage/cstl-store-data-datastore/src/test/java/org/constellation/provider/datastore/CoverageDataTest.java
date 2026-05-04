44d791e2a0aad62242266693859e8662b42b3421
Alexis Manin <alexis.manin@geomatys.com>
2026-05-04 12:08:21 +0200

    refactor(Test): Avoid hard-coded dependency to DefaultCoverageData in coverage data test
    It only "moves" hard-coded dependency, as we explicitely depend on "DefaultExaDataCreator".
    Next level would be to use dependency injection to autowire an arbitrary "ExaDataCreator".

--- modules/storage/cstl-store-data-datastore/src/test/java/org/constellation/provider/datastore/CoverageDataTest.java
+++ modules/storage/cstl-store-data-datastore/src/test/java/org/constellation/provider/datastore/CoverageDataTest.java
@@ -18,25 +18,28 @@
  */
 package org.constellation.provider.datastore;
 
+import com.examind.provider.component.DefaultExaDataCreator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import javax.imageio.ImageIO;
 import org.apache.sis.referencing.CRS;
-import org.apache.sis.storage.Aggregate;
 import org.apache.sis.storage.DataStore;
 import org.apache.sis.storage.GridCoverageResource;
-import org.apache.sis.storage.Resource;
 import org.constellation.dto.BandDescription;
 import org.constellation.dto.CoverageDataDescription;
 import org.constellation.dto.StatInfo;
-import org.constellation.provider.DefaultCoverageData;
+import org.constellation.provider.CoverageData;
+import org.constellation.provider.Data;
 import org.constellation.provider.util.ImageStatisticSerializer;
 import org.constellation.test.utils.TestEnvironment;
 import static org.constellation.test.utils.TestEnvironment.initDataDirectory;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.geotoolkit.storage.DataStores;
 import org.geotoolkit.storage.coverage.ImageStatistics;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.AutoClose;
 import org.opengis.geometry.Envelope;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
@@ -46,8 +49,13 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
  */
 public class CoverageDataTest {
 
-    private static DefaultCoverageData martinique;
-    private static DefaultCoverageData sst;
+    @AutoClose
+    private static DataStore martiniqueStore;
+    private static CoverageData martinique;
+    
+    @AutoClose
+    private static DataStore sstStore;
+    private static CoverageData sst;
 
     @BeforeClass
     public static void init() throws Exception {
@@ -56,17 +64,25 @@ public class CoverageDataTest {
         org.geotoolkit.lang.Setup.initialize(null);
 
         final TestEnvironment.TestResources testResource = initDataDirectory();
-        DataStore store = testResource.createStore(TestEnvironment.TestResource.TIF);
+        martiniqueStore = testResource.createStore(TestEnvironment.TestResource.TIF);
+        var storeCoverages = DataStores.flatten(martiniqueStore, true, GridCoverageResource.class);
+        assertNotNull(storeCoverages);
+        assertEquals(1, storeCoverages.size(), "Expect exactly one coverage in martinique.tif");
+        if (new DefaultExaDataCreator().create("martinique", null, martiniqueStore, storeCoverages.iterator().next()) instanceof CoverageData martinique) {
+            CoverageDataTest.martinique = martinique;
+        } else {
+            fail("martinique.tif has not been imported as a Coverage data");
+        }
 
-        Assert.assertTrue(store instanceof Aggregate);
-        Resource r = ((Aggregate) store).components().iterator().next();
-        Assert.assertTrue(r instanceof GridCoverageResource gcr);
-        martinique = new DefaultCoverageData(r.getIdentifier().get(), (GridCoverageResource) r, store);
-
-        store = testResource.createStore(TestEnvironment.TestResource.PNG);
-        r = store.findResource("SSTMDE200305");
-        Assert.assertTrue(r instanceof GridCoverageResource);
-        sst = new DefaultCoverageData(r.getIdentifier().get(), (GridCoverageResource)r, store);
+        sstStore = testResource.createStore(TestEnvironment.TestResource.PNG);        
+        storeCoverages = DataStores.flatten(sstStore, true, GridCoverageResource.class);
+        assertNotNull(storeCoverages);
+        assertEquals(1, storeCoverages.size(), "Expect exactly one coverage in SSTMDE200305.png");
+        if (new DefaultExaDataCreator().create("sst", null, sstStore, storeCoverages.iterator().next()) instanceof CoverageData sst) {
+            CoverageDataTest.sst = sst;
+        } else {
+            fail("SSTMDE200305.png has not been imported as a Coverage data");
+        }
     }
 
     @Test
@@ -75,87 +91,87 @@ public class CoverageDataTest {
         CoordinateReferenceSystem crs = CRS.forCode("EPSG:3857");
 
         Envelope env = martinique.getEnvelope();
-        Assert.assertNotNull(env);
-        Assert.assertEquals(-61.61, env.getMinimum(0),0.1);
-        Assert.assertEquals( 14.25, env.getMinimum(1),0.1);
-        Assert.assertEquals(-60.69, env.getMaximum(0),0.1);
-        Assert.assertEquals( 15.02, env.getMaximum(1),0.1);
+        assertNotNull(env);
+        assertEquals(-61.61, env.getMinimum(0),0.1);
+        assertEquals( 14.25, env.getMinimum(1),0.1);
+        assertEquals(-60.69, env.getMaximum(0),0.1);
+        assertEquals( 15.02, env.getMaximum(1),0.1);
 
         env = martinique.getEnvelope(crs);
-        Assert.assertNotNull(env);
-        Assert.assertEquals(-6859137.568050235, env.getMinimum(0),0.0001);
-        Assert.assertEquals(1603984.0704114565, env.getMinimum(1),0.0001);
-        Assert.assertEquals(-6756064.723864956, env.getMaximum(0),0.0001);
-        Assert.assertEquals(1692569.0006932162, env.getMaximum(1),0.0001);
+        assertNotNull(env);
+        assertEquals(-6859137.568050235, env.getMinimum(0),0.0001);
+        assertEquals(1603984.0704114565, env.getMinimum(1),0.0001);
+        assertEquals(-6756064.723864956, env.getMaximum(0),0.0001);
+        assertEquals(1692569.0006932162, env.getMaximum(1),0.0001);
 
         env = sst.getEnvelope();
-        Assert.assertNotNull(env);
+        assertNotNull(env);
 
-        Assert.assertEquals(-180.0, env.getMinimum(0) ,0.2);
-        Assert.assertEquals( -90.0, env.getMinimum(1) ,0.2);
-        Assert.assertEquals( 180.0, env.getMaximum(0) ,0.2);
-        Assert.assertEquals(  90.0, env.getMaximum(1) ,0.2);
+        assertEquals(-180.0, env.getMinimum(0) ,0.2);
+        assertEquals( -90.0, env.getMinimum(1) ,0.2);
+        assertEquals( 180.0, env.getMaximum(0) ,0.2);
+        assertEquals(  90.0, env.getMaximum(1) ,0.2);
 
         env = sst.getEnvelope(crs);
-        Assert.assertNotNull(env);
+        assertNotNull(env);
 
-        Assert.assertEquals(-20057076.22203025,       env.getMinimum(0),0.0001);
-        Assert.assertEquals(-41329615.42378936,       env.getMinimum(1),0.0001);
-        Assert.assertEquals( 20017940.463548236,      env.getMaximum(0),0.0001);
-        Assert.assertEquals(Double.POSITIVE_INFINITY, env.getMaximum(1),0.0001);
+        assertEquals(-20057076.22203025,       env.getMinimum(0),0.0001);
+        assertEquals(-41329615.42378936,       env.getMinimum(1),0.0001);
+        assertEquals( 20017940.463548236,      env.getMaximum(0),0.0001);
+        assertEquals(Double.POSITIVE_INFINITY, env.getMaximum(1),0.0001);
     }
 
     @Test
     public void testGetDataDescription() throws Exception {
         StatInfo info = getStatInfo(martinique);
         CoverageDataDescription result = martinique.getDataDescription(info, martinique.getEnvelope());
-        Assert.assertNotNull(result);
+        assertNotNull(result);
 
-        Assert.assertNotNull(result.getBoundingBox());
-        Assert.assertEquals(-61.61, result.getBoundingBox()[0],0.1);
-        Assert.assertEquals( 14.25, result.getBoundingBox()[1],0.1);
-        Assert.assertEquals(-60.69, result.getBoundingBox()[2],0.1);
-        Assert.assertEquals( 15.02, result.getBoundingBox()[3],0.1);
+        assertNotNull(result.getBoundingBox());
+        assertEquals(-61.61, result.getBoundingBox()[0],0.1);
+        assertEquals( 14.25, result.getBoundingBox()[1],0.1);
+        assertEquals(-60.69, result.getBoundingBox()[2],0.1);
+        assertEquals( 15.02, result.getBoundingBox()[3],0.1);
 
-        Assert.assertNotNull(result.getBands());
-        Assert.assertEquals(3, result.getBands().size());
+        assertNotNull(result.getBands());
+        assertEquals(3, result.getBands().size());
 
         BandDescription desc = getBand("Red", result);
-        Assert.assertNotNull(desc);
-        Assert.assertEquals("0", desc.getIndice());
-        Assert.assertEquals(15,  desc.getMinValue(), 0.1);
-        Assert.assertEquals(253, desc.getMaxValue(), 0.1);
-        Assert.assertArrayEquals(new double[]{Double.NaN}, desc.getNoDataValues(), 0);
+        assertNotNull(desc);
+        assertEquals("0", desc.getIndice());
+        assertEquals(15,  desc.getMinValue(), 0.1);
+        assertEquals(253, desc.getMaxValue(), 0.1);
+        assertArrayEquals(new double[]{Double.NaN}, desc.getNoDataValues(), 0);
 
         info = getStatInfo(sst);
         result = sst.getDataDescription(info, sst.getEnvelope());
-        Assert.assertNotNull(result);
+        assertNotNull(result);
 
-        Assert.assertNotNull(result.getBoundingBox());
-        Assert.assertEquals( -180.0, result.getBoundingBox()[0], 0.2);
-        Assert.assertEquals(  -90.0, result.getBoundingBox()[1], 0.2);
-        Assert.assertEquals(  180.0, result.getBoundingBox()[2], 0.2);
-        Assert.assertEquals(   90.0, result.getBoundingBox()[3], 0.2);
+        assertNotNull(result.getBoundingBox());
+        assertEquals( -180.0, result.getBoundingBox()[0], 0.2);
+        assertEquals(  -90.0, result.getBoundingBox()[1], 0.2);
+        assertEquals(  180.0, result.getBoundingBox()[2], 0.2);
+        assertEquals(   90.0, result.getBoundingBox()[3], 0.2);
 
-        Assert.assertNotNull(result.getBands());
-        Assert.assertEquals(1, result.getBands().size());
+        assertNotNull(result.getBands());
+        assertEquals(1, result.getBands().size());
 
         desc = getBand("Color index", result);
-        Assert.assertNotNull(desc);
-        Assert.assertEquals("0",   desc.getIndice());
-        Assert.assertEquals(  0.0, desc.getMinValue(), 0);
-        Assert.assertEquals(224.0, desc.getMaxValue(), 0);
-        Assert.assertArrayEquals(new double[0], desc.getNoDataValues(), 0);
+        assertNotNull(desc);
+        assertEquals("0",   desc.getIndice());
+        assertEquals(  0.0, desc.getMinValue(), 0);
+        assertEquals(224.0, desc.getMaxValue(), 0);
+        assertArrayEquals(new double[0], desc.getNoDataValues(), 0);
 
     }
 
     @Test
     public void testGetImageFormat() throws Exception {
         // only world coverage file support this for now
-        Assert.assertFalse(martinique.getImageFormat().isPresent());
+        assertFalse(martinique.getImageFormat().isPresent());
 
-        Assert.assertTrue(sst.getImageFormat().isPresent());
-        Assert.assertEquals("image/png", sst.getImageFormat().get());
+        assertTrue(sst.getImageFormat().isPresent());
+        assertEquals("image/png", sst.getImageFormat().get());
     }
 
     private static BandDescription getBand(String name, CoverageDataDescription desc)  {
@@ -165,8 +181,10 @@ public class CoverageDataTest {
         return null;
     }
 
-    private StatInfo getStatInfo(DefaultCoverageData data) throws Exception  {
-        ImageStatistics stat = data.computeStatistic(-1, null);
+    private StatInfo getStatInfo(Data<GridCoverageResource> data) throws Exception  {
+        if (!(data.computeStatistic(-1, null) instanceof ImageStatistics stat)) {
+            throw new AssertionError("Image statistics not computed");
+        }
         final ObjectMapper mapper = new ObjectMapper();
         final SimpleModule module = new SimpleModule();
         module.addSerializer(ImageStatistics.class, new ImageStatisticSerializer());
